0849d0f379a9eac8cc4241f54ebe0e7a82812f54
ryutaroda <56154082+ryutaroda@users.noreply.github.com>
2023-04-26 00:58:54 +0900

    修正.

--- components/page/Login.tsx
+++ components/page/Login.tsx
@@ -33,7 +33,7 @@ export const Login = () => {
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#" onClick={(e) => handleSubmit(e)}>
+                        <form className="space-y-4 md:space-y-6" onClick={(e) => handleSubmit(e)}>
                             <div>
                                 <label htmlFor="email"
                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
@@ -41,7 +41,7 @@ export const Login = () => {
                                 <input type="email" name="email" id="email"
                                        value={email} onChange={e => setEmail(e.target.value)}
                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="name@company.com" required=""/>
+                                       placeholder="name@company.com" required/>
                             </div>
                             <div>
                                 <label htmlFor="password"
@@ -49,7 +49,7 @@ export const Login = () => {
                                 <input type="password" name="password" id="password" placeholder="••••••••"
                                        value={password} onChange={e => setPassword(e.target.value)}
                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       required=""/>
+                                       required/>
                             </div>
 
                             <button className="btn w-full" type="submit">Sign in</button>
